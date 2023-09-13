c218a1ee29e8ffdda01a540ce7946768048b2065
carloszaep <106843133+carloszaep@users.noreply.github.com>
2023-09-13 19:22:01 -0400

    finish first project

--- 03-pizza-menu/pizza-menu/src/App.jsx
+++ 03-pizza-menu/pizza-menu/src/App.jsx
@@ -14,12 +14,12 @@ function App() {
 
 function Pizza({ name, price, ingredient, photoName }) {
   return (
-    <div>
-      <img src="https://www.allrecipes.com/thmb/_Bm6DQpYEYMj3x9VRsGuhr31Y2A=/0x512/filters:no_upscale():max_bytes(150000):strip_icc()/23181spaghetti-pizza-iSoupLovingNicole-700a1af7b7954db9b9cb63330be1d08f.jpg" alt={photoName} />
+    <li>
+      <img width={300} src="https://www.allrecipes.com/thmb/_Bm6DQpYEYMj3x9VRsGuhr31Y2A=/0x512/filters:no_upscale():max_bytes(150000):strip_icc()/23181spaghetti-pizza-iSoupLovingNicole-700a1af7b7954db9b9cb63330be1d08f.jpg" alt={photoName} />
       <h3>{name}</h3>
       <p>{ingredient}</p>
       <p>{price}</p>
-    </div>
+    </li>
   )
 }
 
@@ -31,20 +31,43 @@ function Header() {
   )
 }
 
+const PIZZAS = [
+  {
+    name: 'Pizza Spagetti',
+    ingredient: 'tomato, peperoni',
+    price: 33
+  },
+  {
+    name: 'Pizza wwi',
+    ingredient: 'tomato, peperoni',
+    price: 311
+  },
+  {
+    name: 'Pizza wwwwwtti',
+    ingredient: 'tomato, peperoni',
+    price: 22
+  }
+]
+
 function Menu() {
   return (
     <main>
       <h2>Our Menu</h2>
-      <Pizza name='Pizza Spagetti' ingredient='tomato, peperoni' price={33} />
+      <ul>
+        {PIZZAS.map((pizza, index) => <Pizza key={index} name={pizza.name} ingredient={pizza.ingredient} price={pizza.price} />)}
+
+      </ul>
+      <button>Order now</button>
     </main>
   )
 }
 
 function Footer() {
   const hour = new Date().getHours()
-  const openHour = 12
+  const openHour = 19
   const closeHour = 22
-  const isOpen = hour >= openHour && hour <= closeHour
+  const isOpen = hour >= openHour && hour < closeHour
+
 
   return (
     <div>{new Date().toLocaleTimeString()}. {isOpen ? 'We are currently open' : 'We are close'}</div>
