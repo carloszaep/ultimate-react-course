c250d1546658e2cc0ee2c6dc0ffd5310611a01a7
carloszaep <106843133+carloszaep@users.noreply.github.com>
2023-09-27 18:04:04 -0400

    add api call to get movies

--- 07-usepopcorn/usepopcorn/src/main.jsx
+++ 07-usepopcorn/usepopcorn/src/main.jsx
@@ -2,23 +2,14 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import StartRating from './StartRating.jsx'
 
 
-function Test() {
-  const [a, setA] = useState(1)
 
-  return (
-    <>
-      <p>{a}</p>
-      <StartRating onSetRatingHandler={setA} />
-    </>
-  )
-}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Test />
+    <App />
 
   </React.StrictMode>,
 )
