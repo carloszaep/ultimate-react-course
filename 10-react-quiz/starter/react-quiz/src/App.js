6a198abdd31654862ab4cf6e37658eb0cf4c5b02
carloszaep <106843133+carloszaep@users.noreply.github.com>
2023-10-16 19:25:42 -0400

    start react quiz project

---
+++ 10-react-quiz/starter/react-quiz/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Main from "./Main";
+
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main className="main">
+        <p>1/15</p>
+        <p>question?</p>
+      </Main>
+    </div>
+  );
+}
+
+export default App;
