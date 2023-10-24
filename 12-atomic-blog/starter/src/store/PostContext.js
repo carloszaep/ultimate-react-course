093899d9311a6b6209093d032fe9efc0b80157ae
carloszaep <106843133+carloszaep@users.noreply.github.com>
2023-10-24 19:16:01 -0400

    add context to blogs

---
+++ 12-atomic-blog/starter/src/store/PostContext.js
@@ -0,0 +1,51 @@
+import { useState, createContext, useContext } from 'react'
+import createRandomPost from '../util/createRandomPost';
+
+
+// 1 create a context
+// 2 provide value to child component
+// useContext in the consumer component 
+export const PostContext = createContext()
+
+
+
+
+export function PostProvider({ children }) {
+    const [posts, setPosts] = useState(() =>
+        Array.from({ length: 30 }, () => createRandomPost())
+    );
+    const [searchQuery, setSearchQuery] = useState("");
+
+
+    // Derived state. These are the posts that will actually be displayed
+    const searchedPosts =
+        searchQuery.length > 0
+            ? posts.filter((post) =>
+                `${post.title} ${post.body}`
+                    .toLowerCase()
+                    .includes(searchQuery.toLowerCase())
+            )
+            : posts;
+
+    function handleAddPost(post) {
+        setPosts((posts) => [post, ...posts]);
+    }
+
+    function handleClearPosts() {
+        setPosts([]);
+    }
+
+    return <PostContext.Provider value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery, setSearchQuery
+    }}>{children}</PostContext.Provider>
+}
+
+
+export function usePosts() {
+    const context = useContext(PostContext)
+    return context
+}
+
