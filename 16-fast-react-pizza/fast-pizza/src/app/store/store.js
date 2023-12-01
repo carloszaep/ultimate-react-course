fffe344bc258bb7ccfd68319941013a3fa2836f0
carloszaep <106843133+carloszaep@users.noreply.github.com>
2023-11-30 19:24:19 -0500

    connect the cart reducer whit the global store

--- 16-fast-react-pizza/fast-pizza/src/app/store/store.js
+++ 16-fast-react-pizza/fast-pizza/src/app/store/store.js
@@ -2,10 +2,12 @@
 
 import { configureStore } from "@reduxjs/toolkit"
 import userReducer from "../user/userSlice";
+import cartReducer from "../cart/cartSlice";
 
 const store = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        cart: cartReducer
     }
 })
 
